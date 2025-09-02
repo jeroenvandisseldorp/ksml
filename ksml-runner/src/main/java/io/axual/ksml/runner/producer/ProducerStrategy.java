72e5b262389a2a46b03ff0e425d7f956572e780b
jeroenvandisseldorp <jeroen.van.disseldorp@axual.com>
2025-09-02 07:54:40 +0200

    Small update for code compactness

--- ksml-runner/src/main/java/io/axual/ksml/runner/producer/ProducerStrategy.java
+++ ksml-runner/src/main/java/io/axual/ksml/runner/producer/ProducerStrategy.java
@@ -60,14 +60,7 @@ public class ProducerStrategy {
         }
 
         // Set the message count
-        if (definition.messageCount() != null) {
-            if (definition.messageCount() >= 1)
-                messageCount = definition.messageCount();
-            else
-                messageCount = 1;
-        } else {
-            messageCount = INFINITE;
-        }
+        messageCount = definition.messageCount() != null ? Math.max(definition.messageCount(), 1) : INFINITE;
 
         // Set the batch size
         if (definition.batchSize() != null) {
@@ -92,13 +85,10 @@ public class ProducerStrategy {
     }
 
     private UserPredicate userPredicateFrom(FunctionDefinition function, PythonContext context, String namespace, String name, MetricTags tags) {
-        if (function == null) {
-            return null;
-        }
-        if (function.name() != null) {
-            return new UserPredicate(PythonFunction.forPredicate(context, namespace, function.name(), function), tags);
-        }
-        return new UserPredicate(PythonFunction.forPredicate(context, namespace, name, function), tags);
+        if (function == null) return null;
+        return new UserPredicate(
+                PythonFunction.forPredicate(context, namespace, function.name() != null ? function.name() : name, function),
+                tags);
     }
 
     public boolean shouldReschedule() {
